9fb138c471d20aaa05f7a33dc7ede5c5c038afa3
wilmacedo <wil.macedo.sa@gmail.com>
2021-04-15 20:50:38 -0300

    Finish parse and count confirmed and unconfirmed values

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"os"
+	"strconv"
 )
 
 type Transaction struct {
@@ -16,23 +17,50 @@ type Transaction struct {
 	Confirmations int
 }
 
+type Result struct {
+	Confirmed int
+	Unconfirmed int
+}
+
 func GetBalance(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	
 	var transactions []Transaction
+	var result Result
 
 	response, err := http.Get(os.Getenv("API_URL") + mux.Vars(r)["address"])
 
 	if err == nil {
 		data,_ := ioutil.ReadAll(response.Body)
 		json.Unmarshal([]byte(data), &transactions)
-		json.NewEncoder(w).Encode(transactions)
+
+		var length = len(transactions)
+
+		if (length == 0) {
+			handleError(w)
+			return
+		}
+		
+		for i := 0; i < length; i++ {
+			res,_ := strconv.Atoi(transactions[i].Value)
+			if transactions[i].Confirmations > 2 {
+				result.Confirmed += res
+			} else {
+				result.Unconfirmed += res
+			}
+		}
+
+		json.NewEncoder(w).Encode(result)
 	} else {
-		jsonData := map[string]string{"error": "Wallet not found."}
-		jsonValue,_ := json.Marshal(jsonData)
-		w.WriteHeader(http.StatusNotFound)
-		w.Write(jsonValue)
+		handleError(w)
 	}
+}
+
+func handleError(w http.ResponseWriter) {
+	jsonData := map[string]string{"error": "Wallet not found."}
+	jsonValue,_ := json.Marshal(jsonData)
+	w.WriteHeader(http.StatusNotFound)
+	w.Write(jsonValue)
 }
 
 func main() {
