76520ccc845574559e85bdc89bf6ed3c748149f2
wilmacedo <wil.macedo.sa@gmail.com>
2021-04-15 20:08:10 -0300

    Create route and parse response body

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
+	"encoding/json"
 	"net/http"
-	"math/rand"
-	"strconv"
+	"io/ioutil"
 	"github.com/gorilla/mux"
 )
 
-type Wallet struct {
-	ID string `json:"id"`
+type Transaction struct {
+	TXID string
+	Value string
+	Confirmations int
+}
+
+func GetBalance(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	
+	// transactionsJson := `[{"txid":"74772f6296638990cbb6107792ad370a5bc0a878c649cc763d42506e5178acdf","vout":0,"value":"13394","confirmations":0,"lockTime":672872}]`
+	var transactions []Transaction
+
+	response, err := http.Get("https://blockbosok-bitcoin.tronwallet.me/api/v2/utxo/1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2")
+
+	if err == nil {
+		data,_ := ioutil.ReadAll(response.Body)
+		json.Unmarshal([]byte(data), &transactions)
+		json.NewEncoder(w).Encode(transactions)
+	} else {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
 }
 
 func main() {
@@ -19,8 +37,4 @@ func main() {
 	router.HandleFunc("/balance/{address}", GetBalance).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func GetBalance(w http.ResponseWriter, r *http.Request) {
-
 }
