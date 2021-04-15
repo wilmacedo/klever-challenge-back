b7169ab322a6da7fc83906e0ffc86edd24aeb021
wilmacedo <wil.macedo.sa@gmail.com>
2021-04-15 20:25:39 -0300

    Finish params and handle errors

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"io/ioutil"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 type Transaction struct {
@@ -17,21 +19,28 @@ type Transaction struct {
 func GetBalance(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	
-	// transactionsJson := `[{"txid":"74772f6296638990cbb6107792ad370a5bc0a878c649cc763d42506e5178acdf","vout":0,"value":"13394","confirmations":0,"lockTime":672872}]`
 	var transactions []Transaction
 
-	response, err := http.Get("https://blockbosok-bitcoin.tronwallet.me/api/v2/utxo/1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2")
+	response, err := http.Get(os.Getenv("API_URL") + mux.Vars(r)["address"])
 
 	if err == nil {
 		data,_ := ioutil.ReadAll(response.Body)
 		json.Unmarshal([]byte(data), &transactions)
 		json.NewEncoder(w).Encode(transactions)
 	} else {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		jsonData := map[string]string{"error": "Wallet not found."}
+		jsonValue,_ := json.Marshal(jsonData)
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(jsonValue)
 	}
 }
 
 func main() {
+	err := godotenv.Load()
+  if err != nil {
+    log.Fatal(err)
+  }
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/balance/{address}", GetBalance).Methods("GET")
